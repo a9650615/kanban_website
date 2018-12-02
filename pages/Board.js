686091ce9e5fd002dc9b52df5e9bbbe5a7066c31
菜鳥祐 <a9650615@gmail.com>
2018-12-02 16:32:11 +0800

    Boards cards into components

--- pages/Board.js
+++ pages/Board.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Head from "next/head";
-import Board from "react-trello";
-import styled from "styled-components";
+import "react-trello";
+import "styled-components";
 
 import { withSSR } from "./_ssr";
 
 import Page, { Wrapper } from "../components/Page";
 import { H1 } from "../components/Headers";
 import TopBar from "../components/TopBar";
+import { StyledBoard, LaneHeader, CustomCard } from "../components/TaskCard";
 
 const data = {
   lanes: [
@@ -69,78 +70,6 @@ const data = {
     },
   ],
 };
-const LaneHeader = props => (
-  <div style={{ margin: "25px 25px 0px 25px", fontSize: 20, fontWeight: 500 }}>
-    {props.title}
-  </div>
-);
-const StyledBoard = styled(Board)`
-  .smooth-dnd-container.horizontal > .smooth-dnd-draggable-wrapper {
-    background: #fff;
-    min-height: 555px;
-    box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 6px;
-    border-radius: 10px;
-    margin-right: 50px;
-    min-width: 340px;
-    position: relative;
-  }
-  .smooth-dnd-container.horizontal {
-    max-width: 50em;
-    margin-left: auto;
-    margin-right: auto;
-  }
-  .smooth-dnd-container.horizontal > .smooth-dnd-draggable-wrapper > section {
-    height: 100%;
-  }
-  .smooth-dnd-container.vertical {
-    padding: 0px 0px 200px 0px;
-    min-height: 200px;
-    padding: 0px 20px 20px 20px;
-  }
-`;
-const CardWrapper = styled("div")`
-  background: #fff;
-  padding: 20px;
-  border: 0.1px solid #dddddd;
-  border-top: 2px solid ${props => props.cardColor || "#dddddd"};
-  width: 300px;
-  min-height: 100px;
-  margin-top: 20px;
-  position: relative;
-`;
-const CustomCard = props => (
-  <CardWrapper {...props}>
-    <header
-      style={{
-        paddingBottom: 6,
-        marginBottom: 10,
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        // color: props.cardColor,
-      }}
-    >
-      <div style={{ fontSize: 20, fontWeight: "bold" }}>{props.title}</div>
-      <div
-        style={{
-          fontSize: 15,
-          color: props.typeColor,
-          borderRight: `3px solid ${props.typeColor}`,
-          position: "absolute",
-          right: "0",
-          paddingRight: 10,
-        }}
-      >
-        {props.status}
-      </div>
-    </header>
-    <div style={{ fontSize: 14 }}>
-      <div style={{ padding: "5px 0px" }}>
-        <div>{props.body}</div>
-      </div>
-    </div>
-  </CardWrapper>
-);
 
 class HomeScreen extends React.Component {
   state = {
